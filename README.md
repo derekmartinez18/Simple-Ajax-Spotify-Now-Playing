3f4685250b7e471c75b20c10b7ea1b3381d23bf5
Derek Martinez <me@derek-martinez.us>
2013-04-29 01:46:54 -0400

    A version update

---
+++ README.md
@@ -0,0 +1,44 @@
+AJAX and Last.FM powered Spotify plugin
+==================
+
+This is a simple plugin created to display what you are currently listening to on Spotify (www.spotify.com) using Last.FM (www.last.fm). 
+
+Getting Started
+--------------
+
+**This plugin requires the following:**
+- Last.FM Account with registered API
+- Website with PHP
+- Spotify Account (any type)
+- Spotify client scrobbling to Last.FM
+
+Jquery/Ajax
+--------------
+
+**You need this code and call it when document loads**
+
+	function get_spotify() {
+		$.ajax({
+			type: 'POST',
+			url: './last.fm.php',
+			data: { request: 'true' },
+			success: function(reply) {
+				$('.now-playing').html("<p>" + reply +" on Spotify</p>");
+			}
+		});
+	}
+	
+*You can create your own if you want, this was something I wrote quickly*
+
+Configuring
+--------------
+**Last.FM.php**
+*$user = "your last.fm username here";* kind of explains it self, in the qoutes add your last.fm username.
+*$key = "your api key here";* again, self explained you'll get a key from Last.FM API place it here.
+*$short_titles = false;* If true will trim certain additional text
+
+Updates
+--------------
+[4/29] 
+- Added trimming of useless text like "Album Version", "Explicit Version", "Live" and "(blah blah blah)"
+- Cleaned Code
--- last.fm.php.php
+++ last.fm.php.php
@@ -4,6 +4,8 @@ $user = "your last.fm username here"; // <---- Your username goes here
 $key = "your api key here"; //<-- Your API key goes here
 // The URL of the request to API Service
 $url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$user&api_key=$key&format=json";
+// Enable Shortening
+$short_titles = false;
 // Setup cURL for request
 $ch = curl_init( $url );
 // Options for cURL
@@ -20,9 +22,13 @@ $data = json_decode($json,true);
 // Get only the latest track information
 $last = $data['recenttracks']['track'][0];
 // Is now playing attribute present?
-if( isset($last['@attr']['nowplaying']) ) {
+if (isset($last['@attr']['nowplaying'])) {
 	// Echo now playing information.
-	echo "I am currently listening to " . $last['name'] . ' by ' . $last['artist']['#text'] . " on Spotify.";
+	$output = "I am currently listening to " . $last['name'] . ' by ' . $last['artist']['#text'] . " on Spotify.";
+	if ($short_titles) {
+		$output = sTrim($output);
+	}
+	echo trim($output);
 } else {
 	// Collect info on when this was last played and get the current UNIX time.
 	$played = $last['date']['uts']; $now = time();
@@ -33,14 +39,29 @@ if( isset($last['@attr']['nowplaying']) ) {
 	// format minutes
 	$minutes = intval(($diff / 60) % 60);
 	// IF hours is empty(equal to zero) 
-	if( !empty($hours) ) {
-		if( $hours > 24 ) // Is it more than 1 day?
+	if (!empty($hours)) {
+		if ($hours > 24) // Is it more than 1 day?
 			$time = "over a day ago";
 		else
 			$time = $hours . " hours and " . $minutes . " minutes ago.";
 	} else
 		$time = $minutes . " minutes ago";
-	echo "I last listened to " . $last['name'] . ' by ' . $last['artist']['#text'] . " $time on Spotify";
+	$output = "I last listened to " . $last['name'] . ' by ' . $last['artist']['#text'] . " $time on Spotify";
+	if ($short_titles) {
+		$output = sTrim($output);
+	}
+	echo trim($output);
 }
 exit();
+
+function sTrim($output) {
+	$output = preg_replace("/- Live/i", "", $output);
+	$output = preg_replace("/- Album Version Explicit/i", "", $output);
+	$output = preg_replace("/- Explicit Album Version/i", "", $output);
+	$output = preg_replace("/- Explicit Version/i", "", $output);
+	$output = preg_replace("/- Album Version/i", "", $output);
+	$output = preg_replace("/\\[(.*?)\]/i", "", $output);
+	$output = preg_replace("/\\((.*?)\)/i", "", $output);
+	return $output;
+}
 ?>
